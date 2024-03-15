6b491a8434deb0b225d7209bb2b43519f1627833
JorgeAndaur <aenimalabs@gmail.com>
2024-03-15 10:18:21 -0300

    pudo quedar mejor

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,14 @@ function consultaAPI(numHero) {
     $.ajax(settings)
         .done(function (response) {
 
-            mostrarDatos(response)
+            if (response.response == "error") {
 
-            // mostrarGrafico(response)
+                alert("Héroe no encontrado")
+                return
+            }
+
+            mostrarDatos(response)
+            mostrarGrafico(response)
 
 
             //en un principio quise hacer que se cargara en otra pestaña
@@ -25,7 +30,7 @@ function consultaAPI(numHero) {
             console.error("Error en la solicitud: ", status, error);
 
         })
-}
+};
 
 function mostrarDatos(response) {
 
@@ -35,19 +40,18 @@ function mostrarDatos(response) {
     let arregloTrabajo = pasandoaArreglo(response.work)
     let arregloConexiones = pasandoaArreglo(response.connections)
 
-    console.log("lero lero", arregloConexiones[0].y)
-    console.log("waja", arregloTrabajo[0].y)
-    console.log("que es esto", arregloPoderes[1].y)
-
-
     let cardContainer = $('#cardContainer')
 
+    console.log(arregloPoderes[0])
+    console.log(arregloConexiones[0])
+
+    //inyecta la card con los datos traídos desde la api
     cardContainer.append(`
 
-<div class="card" >
+<div class="card bg-dark text-light" >
       <div class="row g-0">
         <div class="col-md-4 d-flex align-items-center">
-          <img src="${response.image.url}" class="img-fluid rounded-start" alt="...">
+          <img id="imagenSuperHero" src="${response.image.url}" class="img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8">
           <div class="card-body  ">
@@ -68,8 +72,26 @@ function mostrarDatos(response) {
 </div>
 `)
 
+    // Referencia al elemento de la imagen
+    let $imagen = $('#imagenSuperHero');
 
-}
+    // Maneja el evento de carga de la imagen
+    $imagen.on('load', function () {
+        console.log('La imagen se ha cargado correctamente.');
+    });
+
+    // Maneja el evento de error de carga de la imagen
+    $imagen.on('error', function () {
+        console.error('Error al cargar la imagen. La imagen no es accesible (404 Not Found).');
+        console.log('URL de la imagen:', $imagen.attr('src'));
+        console.log('Estado de la imagen:', '404 Not Found');
+        //reemplaza la imagen fallida por una imgagen local
+        $imagen.attr('src', './assets/img/sinmanoparaimagen.avif')
+
+    });
+
+
+};
 
 function pasandoaArreglo(arreglo) {
     let datosArreglo = []
@@ -83,7 +105,7 @@ function pasandoaArreglo(arreglo) {
         })
     }
     return datosArreglo
-}
+};
 
 function pasandoaArregloPS(arreglo) {
     let datosXY = [];
@@ -101,36 +123,100 @@ function pasandoaArregloPS(arreglo) {
     };
 
     return datosXY
-}
+};
 
-// function mostrarGrafico(response) {
-//     console.log(response, "nada")
+function mostrarGrafico(response) {
 
-//     let powerstatsArreglo = []
+    let arregloPoderes = pasandoaArregloPS(response.powerstats)
+    
+    if (arregloPoderes.every(poder => poder.y === 0)) {
+        alert('Los valores de intelligence, strength, speed, durability, power y combat son 0. No se puede graficar.');
+        return;
+    }
+    let dataPoints = [];
 
-//     let options = {
-//         title: {text: "Estadísticas de Poder para"},
-//         axisX:{title : "Frutas Tropicales",titleFontSize: 12},
-//         axisY:{title : "Consumo Kg/persona/año",titleFontSize: 12},
-//         data: [  // es un arreglo objetos que contiene tipo grafico y datos a graficar
-//             {type: "pie",
-//             dataPoints: [  // arreglo de objetos con valores de x axis - y axis
-//                             { label: "papaya", y: 23},
-//                             { label: "naranja", y: 15},
-//                             { label: "platano", y: 25},
-//                             { label: "mango", y: 30},
-//                             { label: "guayaba", y: 20},
-//                         ],
-//             },
-//         ],
-//     };
+    for (let i = 0; i < arregloPoderes.length; i++) {
+        dataPoints.push({
+            name: arregloPoderes[i].label,
+            y: arregloPoderes[i].y,
+            // exploded: (i === 0) // Explode el primer indice
+        });
+    }
+    let chart = new CanvasJS.Chart("chartContainer", {
+
+        animationEnabled: true,
+        title: {
+            text: `Estadísticas de Poder para ${response.name}`,
+            fontFamily: "Bangers, system-ui"
+        },
+       
+        theme:"dark2",
+        responsive: true,
+        legend: {
+            cursor: "pointer",
+            itemclick: explodePie,
+            fontFamily: "Bangers, system-ui"
+        },
+        data: [{
+            type: "pie",
+            showInLegend: true,
+            toolTipContent: "{name}: <strong>{y}</strong>",
+            indexLabel: "{name}  ({y})",
+            dataPoints: dataPoints           
+        }]
+    });
+    chart.render();
+};
+
+function explodePie(e) {
+    if (typeof (e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
+        e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
+    } else {
+        e.dataSeries.dataPoints[e.dataPointIndex].exploded = false;
+    }
+    e.chart.render();
+
+};
+
+(function (jQuery) {
+
+    $.fn.fadeOutPlugin = function () {
+        return this.each(function () {
+            $(this).fadeOut(1000);
+        });
+    };
+ 
+
+$.fn.fadeInPlugin = function () {
+    return this.each(function () {
+        $(this).fadeIn(1000);
+    });
+};
 
 
-// }
+}(jQuery));
+
 
 
 window.onload = function () {
     $(document).ready(function () {
+
+        $("#navnav").click(function (event) {
+            event.preventDefault()
+
+            const plugin = $(".fotoform")            
+            plugin.fadeInPlugin()
+            $('#chartContainer').empty()
+            $('#cardContainer').empty()
+
+                      
+           
+
+            
+            // consultaAPI(numHero)
+            // $("#numHeroInput").val('');
+
+        });      
 
 
         $("#butTon").click(function (event) {
@@ -143,47 +229,25 @@ window.onload = function () {
             if (!/^\d+$/.test(numHero)) {
                 // 3.8 Implementar estructuras condicionales para generar alertas cuando existan errores en la búsqueda.
                 alert("Por favor, ingrese un número válido, no sea desagradable")
+                $("#numHeroInput").val('');
                 return
             }
 
-            
+             const plugin = $(".fotoform")            
+            plugin.fadeOutPlugin()
+
+            //vaciar el container de la card cada vez que busque un superhero
             $('#cardContainer').empty()
-            // $('#chartContainer').empty()
+            
+            
+           
 
             consultaAPI(numHero)
             $("#numHeroInput").val('');
 
 
 
-            let grafico;
-
-            let options4 = {
-                title: { text: "Gráfico de TORTA con jQuery CanvasJS" },
-                axisX: { title: "Frutas Tropicales", titleFontSize: 12 },
-                axisY: { title: "Consumo Kg/persona/año", titleFontSize: 12 },
-                data: [
-                    {
-                        type: "pie",
-                        dataPoints: [
-                            { label: "papaya", y: 23 },
-                            { label: "naranja", y: 15 },
-                            { label: "platano", y: 25 },
-                            { label: "mango", y: 30 },
-                            { label: "guayaba", y: 20 },
-                        ],
-                    },
-                ],
-            };
-
-            grafico = $("#chartContainer");
-
-
-            grafico.CanvasJSChart(options4);
-
-          
-
         });
-
     });
 }
 
